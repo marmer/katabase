d714ce721f6b2165fe2cba9c720c3d7a77856518
Mariano Mertinat <m.mertinat@schuetze-consulting.ag>
2020-05-07 18:02:42 +0200

    Bowling

--- src/main/kotlin/io/github/marmer/wurfeToScore.kt
+++ src/main/kotlin/io/github/marmer/wurfeToScore.kt
@@ -1,48 +1,61 @@
 package io.github.marmer
 
-fun wurfeToScore(eingabe: String): Int {
-    return eingabe.mapIndexed { index, input -> toWurfpunkte(index, eingabe) }.sum()
-}
+fun wurfeToScore(eingabe: String): Int = eingabe.mapIndexed { index, input -> toWurfpunkte(index, eingabe) }.sum()
 
 private fun toWurfpunkte(index: Int, eingabe: String): Int = when {
-    WurfTyp.von(eingabe[index]) == WurfTyp.SPARE ->
-        getBasispunkteFuer(eingabe, index) + getAsBonuspunkt(eingabe, index + 1)
+    WurfTyp.isSpare(eingabe[index]) ->
+        getBasispunkteFuer(index, eingabe) + getSpareExtrapunkteFuer(index, eingabe)
 
-    WurfTyp.von(eingabe[index]) == WurfTyp.STRIKE ->
-        getBasispunkteFuer(eingabe, index) + getAsBonuspunkt(eingabe, index + 1) + getAsBonuspunkt(eingabe, index + 2)
+    WurfTyp.isStrike(eingabe[index]) ->
+        getBasispunkteFuer(index, eingabe) + getStrikeExtrapunkteFuer(index, eingabe)
 
-    else -> getBasispunkteFuer(eingabe, index)
+    else -> getBasispunkteFuer(index, eingabe)
 }
 
-fun getAsBonuspunkt(eingabe: String, index: Int): Int = when {
-    isEndspiel(eingabe, index) -> 0
-    else -> getBasispunkteFuer(eingabe, index)
-}
+private fun getSpareExtrapunkteFuer(index: Int, eingabe: String): Int =
+    if (isEndspiel(index, eingabe)) 0 else getBasispunkteFuer(index + 1, eingabe)
 
-fun isEndspiel(eingabe: String, index: Int): Boolean {
-    if (index < eingabe.length + 2)
+private fun getStrikeExtrapunkteFuer(index: Int, spiel: String): Int =
+    if (isEndspiel(index, spiel)) 0
+    else getBasispunkteFuer(index + 1, spiel) + getBasispunkteFuer(index + 2, spiel)
+
+private fun isEndspiel(index: Int, eingabe: String): Boolean {
+    val kleinsterMoeglicherEndspielindex = 9
+    if (index < kleinsterMoeglicherEndspielindex) {
         return false
-    else
-        return true
+    }
+    var halbeFrames = 0
+    for (i in 0 until index) {
+        halbeFrames += if (WurfTyp.isStrike(eingabe[i])) 2 else 1
+    }
+    return halbeFrames >= kleinsterMoeglicherEndspielindex * 2
 }
 
-private fun getBasispunkteFuer(eingabe: String, index: Int): Int = when {
-    WurfTyp.von(eingabe[index]) == WurfTyp.SPARE -> 10 - getBasispunkteFuer(eingabe, index - 1)
+private fun isOutOfRange(index: Int, eingabe: String) = index >= eingabe.length
+private fun isLetzterWurf(index: Int, eingabe: String) = index == eingabe.length - 1
+private fun isVorletzterWurf(index: Int, eingabe: String) = index == eingabe.length - 2
+
+private fun getBasispunkteFuer(index: Int, eingabe: String): Int = when {
     WurfTyp.von(eingabe[index]) == WurfTyp.STRIKE -> 10
+    WurfTyp.von(eingabe[index]) == WurfTyp.SPARE -> 10 - getBasispunkteFuer(index - 1, eingabe)
     WurfTyp.von(eingabe[index]) == WurfTyp.POINTS -> Character.getNumericValue(eingabe[index])
     else -> 0
 }
 
-enum class WurfTyp {
+private enum class WurfTyp {
     STRIKE, SPARE, POINTS, ZERO;
 
     companion object {
         fun von(input: Char) =
             when {
-                input == 'X' -> STRIKE
-                input == '/' -> STRIKE
+                isStrike(input) -> STRIKE
+                isSpare(input) -> SPARE
                 input.isDigit() -> POINTS
                 else -> ZERO
             }
+
+        fun isSpare(input: Char) = input == '/'
+
+        fun isStrike(input: Char) = input == 'X'
     }
 }
