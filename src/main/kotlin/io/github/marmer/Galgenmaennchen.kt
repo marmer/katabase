ac76b87c6ba1496e3131d319d3cb269925ccc3a4
MarMer <1055445+marmer@users.noreply.github.com>
2020-08-26 21:04:05 +0200

    Reset

--- src/main/kotlin/io/github/marmer/Galgenmaennchen.kt
+++ src/main/kotlin/io/github/marmer/Galgenmaennchen.kt
@@ -1,18 +1,5 @@
 package io.github.marmer
 
 data class Galgenmaennchen(val gesuchtesWort: String) {
-    private var charsTried: Set<Char> = emptySet()
-
-    private constructor(gesuchtesWort: String, charsTried: Set<Char>) : this(gesuchtesWort) {
-        this.charsTried = charsTried
-    }
-
-    fun rateBuchstabe(buchstabe: Char): Galgenmaennchen =
-        Galgenmaennchen(gesuchtesWort, charsTried + buchstabe.standardized())
-
-    override fun toString(): String = gesuchtesWort
-        .map { if (charsTried.contains(it.standardized())) it else '-' }
-        .joinToString(separator = "")
-
-    fun Char.standardized() = toUpperCase()
+   
 }
