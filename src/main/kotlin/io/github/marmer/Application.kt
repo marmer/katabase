1fb7741b0191bce8151f36c1504438abfeacac4b
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-04-30 16:41:30 +0200

    first try

--- src/main/kotlin/io/github/marmer/Application.kt
+++ src/main/kotlin/io/github/marmer/Application.kt
@@ -1,5 +1,78 @@
 package io.github.marmer
 
+import kotlin.math.min
+
 fun main(args: Array<String>) {
-    args.forEach { arg -> println("Hello, World: $arg") }
+    umbrechen(args[0], args[1].toInt())
 }
+
+private val SPACE_SIZE = 1
+
+fun umbrechen(text: String, maxZeilenLaenge: Int): String =
+    if (text.isBlank())
+        ""
+    else
+        text
+            .toWords()
+            .toLinesWithMaxLength(maxZeilenLaenge)
+            .map { it.toBlocksatz(maxZeilenLaenge) }
+            .joinToString("\n")
+
+private fun String.toBlocksatz(maxZeilenLaenge: Int): String =
+    if (isFullWordLine())
+        this
+    else {
+        val woerter = split(" ")
+        val anzahlDerWoerter = woerter.size
+        val anzahlFreiraeume = anzahlDerWoerter - 1
+        val gesamtzahlNoetigerLeerzeichen = maxZeilenLaenge - woerter.map { it.length }.sum()
+        val anzahlLeerzeichenProFreiraum = gesamtzahlNoetigerLeerzeichen / anzahlFreiraeume
+
+        if (gesamtzahlNoetigerLeerzeichen == 0)
+            this
+        else {
+            val smallReplacement = getStringWithGivenNumberOfSpaces(anzahlLeerzeichenProFreiraum)
+
+            woerter.mapIndexed { index, word ->
+                word +
+                        if (index < gesamtzahlNoetigerLeerzeichen % anzahlFreiraeume)
+                            " " //additional space
+                        else
+                            ""
+            }.joinToString(smallReplacement)
+        }
+    }
+
+private fun getStringWithGivenNumberOfSpaces(anzahlLeerzeichenProFreiraum: Int) =
+    (1..anzahlLeerzeichenProFreiraum).map { " " }.joinToString("")
+
+
+private fun String.isFullWordLine() = !contains(" ")
+
+private fun List<String>.toLinesWithMaxLength(maxZeilenLaenge: Int): List<String> =
+    fold(emptyList()) { lines, word ->
+        if (lines.isEmpty())
+            listOf(word)
+        else if (lines.last().length + word.length + SPACE_SIZE > maxZeilenLaenge)
+            if (word.length > maxZeilenLaenge)
+                lines + word.splitToChunksWithSize(maxZeilenLaenge)
+            else
+                lines + word
+        else {
+            val lastLine = lines.last()
+            lines.dropLast(SPACE_SIZE) + "$lastLine $word"
+        }
+    }
+
+private fun String.splitToChunksWithSize(maxZeilenLaenge: Int): List<String> =
+    if (isEmpty())
+        emptyList()
+    else
+        listOf(
+            subSequence(
+                0,
+                min(length, maxZeilenLaenge)
+            ).toString()
+        ) + drop(maxZeilenLaenge).splitToChunksWithSize(maxZeilenLaenge)
+
+private fun String.toWords() = trim().split(Regex("\\s+"))
--- src/test/kotlin/io/github/marmer/ApplicationTest.kt
+++ src/test/kotlin/io/github/marmer/ApplicationTest.kt
@@ -1,12 +1,136 @@
 package io.github.marmer
 
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.emptyString
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
+    @Test
+    fun `Text mit kleinerer Laenge als uebergebene Parameter muss nicht umgebrochen werden`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen("tolle Welt", 10)
+
+        // Assertion
+        assertEquals("tolle Welt", result)
+    }
 
     @Test
-    fun helloTest() {
-        main(arrayOf("Hundekuchen", "Mettbrötchen"))
-        HelloJava.someStaticJavaMethod("foo", "bar")
+    fun `Text mit groesserer Laenge als der uebergebene Parameter sollte umgebrochen werden`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen("tolle Welt", 9)
+
+        // Assertion
+        assertEquals(
+            """
+                |tolle
+                |Welt
+            """.trimMargin(), result
+        )
+    }
+
+    @Test
+    fun `Worte nach bestehenden Umbruchen sollen zusammen in eine Zeile kommen, wenn sie hinein passen`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen(
+            """
+                |tolle
+                |Welt
+            """.trimMargin(), 10
+        )
+
+        // Assertion
+        assertEquals("tolle Welt".trimMargin(), result)
+    }
+
+    @Test
+    fun `Wo nichts gegeben wird, kommt auch nichts zurueck`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen(
+            """
+                
+                
+            """.trimMargin(), 10
+        )
+
+        // Assertion
+        MatcherAssert.assertThat(result, `is`(emptyString()))
+    }
+
+    @Test
+    fun `Zu lange Woerter kommen in eine eigene Zeile und werden dort umgebrochen`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen(
+            """
+                |tolle
+                |HundekuchenHundekuchen
+                |blub Welt
+            """.trimMargin(), 10
+        )
+
+        // Assertion
+        assertEquals(
+            """
+                |tolle
+                |Hundekuche
+                |nHundekuch
+                |en    blub
+                |Welt
+            """.trimMargin(), result
+        )
+    }
+
+    @Test
+    fun `blocksatz Auffuellung funktioniert auch fuer ungleichmaessige Auffuellung`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen(
+            "x x x x".trimMargin(), 12
+        )
+
+        // Assertion
+        assertEquals(
+            "x   x   x  x", result
+        )
+    }
+
+    @Test
+    fun `Akzeptanztest`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen(
+            """
+            Es blaut die Nacht,
+            die Sternlein blinken,
+            Schneeflöcklein leis hernieder sinken.
+            """.trimMargin(), 14
+        )
+
+        // Assertion
+        assertEquals(
+            """
+            Es  blaut  die
+            Nacht,     die
+            Sternlein
+            blinken,
+            Schneeflöcklei
+            n         leis
+            hernieder
+            sinken.
+        """.trimIndent(), result
+        )
     }
 }
