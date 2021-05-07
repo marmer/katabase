8678ff53119d3549bee7a077499b08786bcbda78
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-05-07 17:34:55 +0200

    neuer Tag neues Glück

--- src/main/kotlin/io/github/marmer/Application.kt
+++ src/main/kotlin/io/github/marmer/Application.kt
@@ -1,5 +1,26 @@
 package io.github.marmer
 
-fun main(args: Array<String>) {
-    args.forEach { arg -> println("Hello, World: $arg") }
+fun umbrechen(text: String, maxLineLength: Int): String {
+    return text.split(" ")
+        .fold(listOf(emptyList())) { acc: List<List<String>>, currentWord: String ->
+            list(acc, currentWord, maxLineLength)
+        }
+        .map { it.joinToString(" ") }
+        .joinToString("\n")
+}
+
+private fun list(
+    currentLines: List<List<String>>,
+    currentWord: String,
+    maxLineLength: Int
+): List<List<String>> {
+    val currentLastLineWords: List<String> = currentLines.last()
+    val potentialNewLastLineWords: List<String> = currentLastLineWords + currentWord
+    val potentialNewLineLength: Int = potentialNewLastLineWords.joinToString(" ").length
+
+    return if (potentialNewLineLength > maxLineLength) {
+        currentLines.plus<List<String>>(listOf(currentWord))
+    } else {
+        currentLines.dropLast(1).plus<List<String>>(potentialNewLastLineWords)
+    }
 }
--- src/test/kotlin/io/github/marmer/ApplicationTest.kt
+++ src/test/kotlin/io/github/marmer/ApplicationTest.kt
@@ -1,12 +1,50 @@
 package io.github.marmer
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     @Test
-    fun helloTest() {
-        main(arrayOf("Hundekuchen", "Mettbrötchen"))
-        HelloJava.someStaticJavaMethod("foo", "bar")
+    fun `es sollte nicht umgebrochen werden, wenn die Laenge kleiner ist als die angegeben Anzahl an Zeichen`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen("""Aa Bb""", 5)
+
+        // Assertion
+        assertEquals("""Aa Bb""", result)
+    }
+
+    @Test
+    fun `Umbruch auf ganzes Wort bei mehr Zeichen als angegeben`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen("""Aa Bb""", 3)
+
+        // Assertion
+        assertEquals(
+            """
+                |Aa
+                |Bb""".trimMargin(), result
+        )
+    }
+
+    @Test
+    fun `umbrueche sollten auch bei mehreren Woertern pro Zeile möglich sein`() {
+        // Preparation
+
+        // Execution
+        val result = umbrechen("""Aa Bb Cc Dd Ee""", 5)
+
+        // Assertion
+        assertEquals(
+            """
+                |Aa Bb
+                |Cc Dd
+                |Ee
+            """.trimMargin(), result
+        )
     }
 }
