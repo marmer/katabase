d5a883c57e93ff6ee43676668f569ce02e6dd38f
MarMer <MarMer>
2021-08-14 13:57:57 +0200

    intermediate state

--- src/main/kotlin/io/github/marmer/Application.kt
+++ src/main/kotlin/io/github/marmer/Application.kt
@@ -1,5 +1,19 @@
 package io.github.marmer
 
-fun main(args: Array<String>) {
-    args.forEach { arg -> println("Hello, World: $arg") }
-}
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private val dict = BufferedReader(
+    InputStreamReader(BufferedInputStream(object {}.javaClass.getResourceAsStream("/morse.dict")))
+)
+    .readLines()
+    .map { it.split(" ")[1] to it.split(" ")[0] }
+    .toMap()
+
+
+fun translateFromMorse(morse: String): String =
+    dict.get(morse) ?: throw IllegalArgumentException(morse)
+
+
+
--- src/test/kotlin/io/github/marmer/ApplicationTest.kt
+++ src/test/kotlin/io/github/marmer/ApplicationTest.kt
@@ -1,12 +1,19 @@
 package io.github.marmer
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvFileSource
+import kotlin.test.assertEquals
 
 class ApplicationTest {
 
-    @Test
-    fun helloTest() {
-        main(arrayOf("Hundekuchen", "Mettbrötchen"))
-        HelloJava.someStaticJavaMethod("foo", "bar")
+    @CsvFileSource(resources = ["/morse.dict"], delimiter = ' ')
+    @ParameterizedTest
+    fun `Simple translation with smalest timing should be translatable`(symbol: String, morseCode: String) {
+        assertEquals(symbol, translateFromMorse(morseCode))
     }
+
+    // TODO: marmer 14.08.2021 All Single chars
+    // TODO: marmer 14.08.2021 Single Word
+    // TODO: marmer 14.08.2021 Multiple Words
+    // TODO: marmer 14.08.2021 Timing
 }
