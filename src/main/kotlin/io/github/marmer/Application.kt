17601eb7917a4826d2e7ce11bb3bbf440b7c1bbe
MarMer <MarMer>
2021-08-14 22:13:11 +0200

    Single symbol translation

--- src/main/kotlin/io/github/marmer/Application.kt
+++ src/main/kotlin/io/github/marmer/Application.kt
@@ -4,16 +4,17 @@ import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-private val dict = BufferedReader(
-    InputStreamReader(BufferedInputStream(object {}.javaClass.getResourceAsStream("/morse.dict")))
-)
-    .readLines()
-    .map { it.split(" ")[1] to it.split(" ")[0] }
-    .toMap()
-
+private val dict by lazy {
+    BufferedReader(InputStreamReader(BufferedInputStream(object {}.javaClass.getResourceAsStream("/morse.dict"))))
+        .readLines()
+        .map {
+            it.split(" ")[1] to it.split(" ")[0]
+        }
+        .toMap()
+}
 
 fun translateFromMorse(morse: String): String =
-    dict.get(morse) ?: throw IllegalArgumentException(morse)
+    dict[morse] ?: throw IllegalArgumentException(morse)
 
 
 
--- src/test/kotlin/io/github/marmer/ApplicationTest.kt
+++ src/test/kotlin/io/github/marmer/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package io.github.marmer
 
+import org.assertj.core.api.Assumptions.assumeThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvFileSource
 import kotlin.test.assertEquals
@@ -9,11 +10,12 @@ class ApplicationTest {
     @CsvFileSource(resources = ["/morse.dict"], delimiter = ' ')
     @ParameterizedTest
     fun `Simple translation with smalest timing should be translatable`(symbol: String, morseCode: String) {
+        assumeThat(symbol).isNotBlank()
         assertEquals(symbol, translateFromMorse(morseCode))
     }
 
-    // TODO: marmer 14.08.2021 All Single chars
     // TODO: marmer 14.08.2021 Single Word
     // TODO: marmer 14.08.2021 Multiple Words
     // TODO: marmer 14.08.2021 Timing
+    // TODO: marmer 14.08.2021 Unknown code
 }
