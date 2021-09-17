3bfd2e3460f43109af315fb9215b4337a92a50d7
mariano.mertinat <Mariano.Mertinat@gematik.de>
2021-09-17 16:49:26 +0200

    refactoring

--- src/main/java/io/github/marmer/LatinMorseDictionary.java
+++ src/main/java/io/github/marmer/LatinMorseDictionary.java
@@ -6,66 +6,64 @@ import java.util.Optional;
 
 public class LatinMorseDictionary implements MorseDictionary {
 
-  private final Map<String, String> morseDict =
-      new HashMap<>() {
-        private static final long serialVersionUID = -1436510038482402706L;
+  private static final Map<String, String> morseDict =
+      new HashMap<>();
 
-        {
-          put(".-", "A");
-          put("-...", "B");
-          put("-.-.", "C");
-          put("-..", "D");
-          put(".", "E");
-          put("..-.", "F");
-          put("--.", "G");
-          put("....", "H");
-          put("..", "I");
-          put(".---", "J");
-          put("-.-", "K");
-          put(".-..", "L");
-          put("--", "M");
-          put("-.", "N");
-          put("---", "O");
-          put(".--.", "P");
-          put("--.-", "Q");
-          put(".-.", "R");
-          put("...", "S");
-          put("-", "T");
-          put("..-", "U");
-          put("...-", "V");
-          put(".--", "W");
-          put("-..-", "X");
-          put("-.--", "Y");
-          put("--..", "Z");
-          put("-----", "0");
-          put(".----", "1");
-          put("..---", "2");
-          put("...--", "3");
-          put("....-", "4");
-          put(".....", "5");
-          put("-....", "6");
-          put("--...", "7");
-          put("---..", "8");
-          put("----.", "9");
-          put(".-.-.-", ".");
-          put("--..--", ",");
-          put("..--..", "?");
-          put(".----.", "'");
-          put("-.-.--", "!");
-          put("-..-.", "/");
-          put("-.--.", "(");
-          put("-.--.-", ")");
-          put(".-...", "&");
-          put("---...", ":");
-          put("-.-.-.", ";");
-          put("-...-", "=");
-          put(".-.-.", "+");
-          put("-....-", "-");
-          put("..--.-", "_");
-          put("...-..-", "$");
-          put(".--.-.", "@");
-        }
-      };
+  static {
+    morseDict.put(".-", "A");
+    morseDict.put("-...", "B");
+    morseDict.put("-.-.", "C");
+    morseDict.put("-..", "D");
+    morseDict.put(".", "E");
+    morseDict.put("..-.", "F");
+    morseDict.put("--.", "G");
+    morseDict.put("....", "H");
+    morseDict.put("..", "I");
+    morseDict.put(".---", "J");
+    morseDict.put("-.-", "K");
+    morseDict.put(".-..", "L");
+    morseDict.put("--", "M");
+    morseDict.put("-.", "N");
+    morseDict.put("---", "O");
+    morseDict.put(".--.", "P");
+    morseDict.put("--.-", "Q");
+    morseDict.put(".-.", "R");
+    morseDict.put("...", "S");
+    morseDict.put("-", "T");
+    morseDict.put("..-", "U");
+    morseDict.put("...-", "V");
+    morseDict.put(".--", "W");
+    morseDict.put("-..-", "X");
+    morseDict.put("-.--", "Y");
+    morseDict.put("--..", "Z");
+    morseDict.put("-----", "0");
+    morseDict.put(".----", "1");
+    morseDict.put("..---", "2");
+    morseDict.put("...--", "3");
+    morseDict.put("....-", "4");
+    morseDict.put(".....", "5");
+    morseDict.put("-....", "6");
+    morseDict.put("--...", "7");
+    morseDict.put("---..", "8");
+    morseDict.put("----.", "9");
+    morseDict.put(".-.-.-", ".");
+    morseDict.put("--..--", ",");
+    morseDict.put("..--..", "?");
+    morseDict.put(".----.", "'");
+    morseDict.put("-.-.--", "!");
+    morseDict.put("-..-.", "/");
+    morseDict.put("-.--.", "(");
+    morseDict.put("-.--.-", ")");
+    morseDict.put(".-...", "&");
+    morseDict.put("---...", ":");
+    morseDict.put("-.-.-.", ";");
+    morseDict.put("-...-", "=");
+    morseDict.put(".-.-.", "+");
+    morseDict.put("-....-", "-");
+    morseDict.put("..--.-", "_");
+    morseDict.put("...-..-", "$");
+    morseDict.put(".--.-.", "@");
+  }
 
 
   @Override
